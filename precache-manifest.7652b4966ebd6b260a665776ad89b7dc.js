49918f7d85f676d9abc163b8eaf1f16e7b5be310
Ivan <goncharov1995.2@gmail.com>
2020-07-05 12:17:01 +0300

    Updates

---
+++ precache-manifest.7652b4966ebd6b260a665776ad89b7dc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d6c7083bf371ae46defe81321309b5a",
+    "url": "/tz-Studious-waddle/index.html"
+  },
+  {
+    "revision": "2b2981cf86ab19aeb9f1",
+    "url": "/tz-Studious-waddle/static/css/3.2df36208.chunk.css"
+  },
+  {
+    "revision": "48f678cbf154551b2959",
+    "url": "/tz-Studious-waddle/static/css/4.999a78dc.chunk.css"
+  },
+  {
+    "revision": "46da97a8f76b2e3d78ce",
+    "url": "/tz-Studious-waddle/static/css/5.a6548223.chunk.css"
+  },
+  {
+    "revision": "7b63e086cb22c6755be7",
+    "url": "/tz-Studious-waddle/static/css/main.9f6a4c18.chunk.css"
+  },
+  {
+    "revision": "093f119288e82dfece4d",
+    "url": "/tz-Studious-waddle/static/js/2.53e64bec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tz-Studious-waddle/static/js/2.53e64bec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b2981cf86ab19aeb9f1",
+    "url": "/tz-Studious-waddle/static/js/3.194932d8.chunk.js"
+  },
+  {
+    "revision": "48f678cbf154551b2959",
+    "url": "/tz-Studious-waddle/static/js/4.7606eef9.chunk.js"
+  },
+  {
+    "revision": "46da97a8f76b2e3d78ce",
+    "url": "/tz-Studious-waddle/static/js/5.8a35c8cf.chunk.js"
+  },
+  {
+    "revision": "8c2aa2d344a0243474db",
+    "url": "/tz-Studious-waddle/static/js/6.9e3e1e97.chunk.js"
+  },
+  {
+    "revision": "cfc555119dd3b42abacf",
+    "url": "/tz-Studious-waddle/static/js/7.a243bf8d.chunk.js"
+  },
+  {
+    "revision": "7b63e086cb22c6755be7",
+    "url": "/tz-Studious-waddle/static/js/main.2dbbfb24.chunk.js"
+  },
+  {
+    "revision": "28ae87a03edbf7292820",
+    "url": "/tz-Studious-waddle/static/js/runtime-main.6e5e0a4e.js"
+  },
+  {
+    "revision": "de4429593bdaca898840356a06216f62",
+    "url": "/tz-Studious-waddle/static/media/404.de442959.jpg"
+  },
+  {
+    "revision": "2f6d253606675cc72191fddfe2efec7f",
+    "url": "/tz-Studious-waddle/static/media/loading.2f6d2536.gif"
+  },
+  {
+    "revision": "bf05268b34fdd6b846e676d220ba058e",
+    "url": "/tz-Studious-waddle/static/media/logo.bf05268b.svg"
+  },
+  {
+    "revision": "d69743708726d9e737e17b705394ecba",
+    "url": "/tz-Studious-waddle/static/media/profile-photo.d6974370.jpg"
+  }
+]);
